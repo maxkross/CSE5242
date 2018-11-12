fb97fc0e6f257bbe49fc3a90579d05ab61dca8ba
Kamalrajsd <kamalrajsd@gmail.com>
2018-11-12 13:04:16 -0500

    Base commit

---
+++ constants.py
@@ -0,0 +1,6 @@
+DB_NAME = 'pg_pythia'
+DB_HOST = 'localhost'
+DB_USER_NAME = 'postgres'
+DB_PASSWORD = 'postgre'
+POSTGRE_LOC = 'G:\\PostgreSQL\\11\\bin\\'
+DB_CONN_STRING = "host='%s' dbname='%s' user='%s' password='%s'"
